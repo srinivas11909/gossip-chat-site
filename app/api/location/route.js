27374f301b048694de328b1188754b552f1246f9
srinivas11909 <srinivas.reddy865@gmail.com>
2025-03-03 01:26:06 +0530

    message api issue fixed

--- app/api/location/route.js
+++ app/api/location/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req) {
   try {
-    const ip = req.headers.get("x-forwarded-for")?.split(",")[0] || "8.8.8.8"; // ✅ Get the real client IP
+    //const ip = req.headers.get("x-forwarded-for")?.split(",")[0] || "8.8.8.8"; // ✅ Get the real client IP
+    const ip = req.headers.get("cf-connecting-ip") || req.headers.get("x-forwarded-for")?.split(",")[0];
 
+    console.log(ip)
     const response = await fetch(`https://ipwho.is/${ip}`, {
       cache: "no-store", // Prevents caching issues on Vercel
     });
